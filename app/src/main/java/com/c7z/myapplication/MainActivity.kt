8990a25584dfe21e62412e0a933dc452d305d1d2
ChunBaee <mljung10@naver.com>
2023-03-17 22:15:09 +0900

    feat: 프로젝트 화면별 패키징 바텀 네비게이션 연결

--- app/src/main/java/com/c7z/myapplication/MainActivity.kt
+++ app/src/main/java/com/c7z/myapplication/MainActivity.kt
@@ -2,14 +2,40 @@ package com.c7z.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.c7z.myapplication.databinding.ActivityMainBinding
+import com.c7z.myapplication.group.GroupFragment
+import com.c7z.myapplication.home.HomeFragment
+import com.c7z.myapplication.map.MapFragment
+import com.c7z.myapplication.setting.SettingFragment
+import com.c7z.myapplication.todo.TodoFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        binding.mainLayoutBottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.main_bottom_nav_btn_home -> setFragment(HomeFragment())
+                R.id.main_bottom_nav_btn_group -> setFragment(GroupFragment())
+                R.id.main_bottom_nav_btn_map -> setFragment(MapFragment())
+                R.id.main_bottom_nav_btn_todo -> setFragment(TodoFragment())
+                R.id.main_bottom_nav_btn_setting -> setFragment(SettingFragment())
+            }
+            false
+        }
+
+        binding.mainLayoutBottomNav.selectedItemId = R.id.main_bottom_nav_btn_home
+    }
+
+    private fun setFragment(fragment : Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.main_layout_container, fragment).commit()
     }
 }
