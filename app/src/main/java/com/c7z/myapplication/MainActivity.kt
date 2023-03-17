5def1d89d935c2c9cef0b4833cc4c502044dfd28
ChunBaee <mljung10@naver.com>
2023-03-17 21:24:19 +0900

    init: 프로젝트 초기 설정

---
+++ app/src/main/java/com/c7z/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.c7z.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c7z.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}
