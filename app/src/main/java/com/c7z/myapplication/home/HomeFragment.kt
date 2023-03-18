50cb838dd10b9536694b56fdfbec7cce47ec5846
ChunBaee <mljung10@naver.com>
2023-03-18 21:36:57 +0900

    feat: HomeFragment 현재 연/월 텍스트 추가 및 Gradle Dependency 버전 오류 해결

--- app/src/main/java/com/c7z/myapplication/home/HomeFragment.kt
+++ app/src/main/java/com/c7z/myapplication/home/HomeFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.c7z.myapplication.R
 import com.c7z.myapplication.databinding.FragmentHomeBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HomeFragment : Fragment() {
     private lateinit var binding : FragmentHomeBinding
@@ -14,8 +18,25 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        initBinding()
+
         return binding.root
+    }
+
+    private fun initBinding() {
+        binding.home = this
+    }
+
+    fun returnCurrentYearMonth() : String = "${getCurrentYear()}년 ${getCurrentMonth()}월"
+
+    private fun getCurrentYear(): String {
+        return SimpleDateFormat("yyyy", Locale.KOREA).format(Date(System.currentTimeMillis()))
+    }
+
+    private fun getCurrentMonth(): String {
+        return SimpleDateFormat("MM", Locale.KOREA).format(Date(System.currentTimeMillis()))
     }
 }
